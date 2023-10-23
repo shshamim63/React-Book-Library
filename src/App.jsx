8887af6c79de4c8e868bc9c3ca50d97dfd7160f8
shshamim63 <shshamim63@gmail.com>
2023-10-23 22:22:52 +0600

    chore(sync): imported components and states to control modal flow

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,35 @@
+import { useState } from "react";
+
+import { Button } from "react-bootstrap";
+
+import Header from "./components/Header";
+import ModalContainer from "./components/ModalContainer";
+
 function App() {
+  const [showModal, setShowModal] = useState(false);
+
+  function openModal() {
+    setShowModal(true);
+  }
+
+  function closeModal() {
+    setShowModal(false);
+  }
+
   return (
-    <>
-      <div className="p-2">Hello</div>
-    </>
+    <div className="main-container">
+      <div className="container">
+        <Header />
+        <div className="row">
+          <div className="col-md-4 offset-md-4">
+            <Button variant="primary" onClick={openModal}>
+              Add book
+            </Button>
+            <ModalContainer show={showModal} closeModal={closeModal} />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
